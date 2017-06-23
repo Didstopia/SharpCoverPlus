53b45be8d2309e4e843a20bb3c2b2d3e3c5c7477
Pauli Jokela <pauli.jokela@didstopia.com>
2017-06-23 11:21:02 +0300

    Updated .travis.yml

--- build.sh
+++ build.sh
@@ -13,8 +13,9 @@ mono SharpCoverPlus/bin/Debug/SharpCoverPlus.exe instrument travisCoverageConfig
 
 ## TODO: This is broken, probably because the test build step was part of the code itself
 # Run NUnit
-nunit-console SharpCoverPlus.Tests/bin/Debug/ProgramTests.dll
+mono packages/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe SharpCoverPlus.Tests/bin/Debug/SharpCoverPlus.Tests.dll
 
+## FIXME: This is now reporting "Could not find file coverageKnowns"
 ## TOOD: This step might be ok, as it's reporting "Overall coverage was 0%."
 # Run SharpCoverPlus
 mono SharpCoverPlus/bin/Debug/SharpCoverPlus.exe check
